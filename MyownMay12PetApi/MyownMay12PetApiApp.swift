33f73d0aff131d6d19768c585c8d80db8a9cae99
yoon <sukanyat2022a@gmail.com>
2025-05-25 08:20:21 -0400

    Initial Commit

---
+++ MyownMay12PetApi/MyownMay12PetApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyownMay12PetApiApp.swift
+//  MyownMay12PetApi
+//
+//  Created by TS2 on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyownMay12PetApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyownMay12PetApiTests/MyownMay12PetApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyownMay12PetApiTests.swift
+//  MyownMay12PetApiTests
+//
+//  Created by TS2 on 5/25/25.
+//
+
+import Testing
+@testable import MyownMay12PetApi
+
+struct MyownMay12PetApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
