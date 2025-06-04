6476431134c91b8439a57e1ef91e9e9e7890ff68
yoon <sukanyat2022a@gmail.com>
2025-06-04 13:00:09 -0400

    add

--- MyownMay12PetApi/View/HelperViewComponent/InfoSingleBoxCompView.swift
+++ MyownMay12PetApi/View/HelperViewComponent/InfoSingleBoxCompView.swift
@@ -7,18 +7,33 @@
 
 import SwiftUI
 
+
+
 struct InfoSingleBoxCompView: View {
+    
     var titleVar: String
     var descriptionVar: String
+    
+    
     var body: some View {
-        HStack(spacing: 30){
+        
+
+        HStack(alignment: .center, spacing: 15){
             Text(titleVar)
                 .font(.headline)
-                
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.blue)
+                .padding(.leading, 5)
+                .frame(width: UIScreen.main.bounds.width * 0.3)
             Text(descriptionVar)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.green)
         }
+        .frame(maxWidth: .infinity)
+        .frame(width: UIScreen.main.bounds.width * 0.9)
+        .frame(minHeight: 60)
         .background(Color.yellow)
-        .padding(.vertical, 50)
+      
         
     }
 }
--- MyownMay12PetApi/View/HomeFolder/DetailScreenView.swift
+++ MyownMay12PetApi/View/HomeFolder/DetailScreenView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct DetailScreenView: View {
+    
+    struct Pet: Identifiable{
+        let name:String
+        let description:String
+        let id = UUID()
+    }
+    
+    
+    let petArray: [Pet] = [
+        Pet(name: "cat", description: "dog3bg"),
+        Pet(name: "dog", description: "img2"),
+        Pet(name: "bird", description: "img1"),
+        Pet(name: "fish",description: "img2"),
+        Pet(name: "lizard", description: "img1"),
+        ]
     var body: some View {
         VStack {
             TopBarCompView()
             BigSquareCompView()
+            ForEach(petArray) { pet in
+                InfoSingleBoxCompView(titleVar: pet.name, descriptionVar: pet.description)
+            }
         }
         Spacer()
     }
